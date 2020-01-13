f1443f506ff21b00718c45b33e9f60ecb4452d31
AngeloDF <angelodf1995@gmail.com>
2020-01-13 18:43:18 +0100

    NOT STABLE -- Printing file stable, Querying not stable | Fixed procedure printed sequencial number | Fixed different procedure print instead of repeating one ( bug fixed )

--- Search_Operations/Printing.py
+++ Search_Operations/Printing.py
@@ -1,4 +1,8 @@
-def print_threshold_results(self, threshold_results):
+from colorama import Fore, Style, Back
+
+from XML_Operations import Parsing
+
+def print_threshold_results(threshold_results, user_output_results, user_score):
     if not threshold_results:
         print(Style.BRIGHT + Fore.MAGENTA + "\tNo result found")
         return
@@ -6,12 +10,12 @@ def print_threshold_results(self, threshold_results):
     # rimozione duplicati
     threshold_results = [i for n, i in enumerate(threshold_results) if i not in threshold_results[n + 1:]]
 
-    if len(threshold_results) < self.user_output_results:
-        self.user_output_results = len(threshold_results)
+    if len(threshold_results) < user_output_results:
+        user_output_results = len(threshold_results)
 
     print()
     i = 0
-    while i < self.user_output_results and len(threshold_results) > 0:
+    while i < user_output_results and len(threshold_results) > 0:
         venue_i = threshold_results[0]["v"]
         pub_i = threshold_results[0]["p"]
         if venue_i != None:
@@ -21,51 +25,53 @@ def print_threshold_results(self, threshold_results):
                 if pub_j != None and "crossref" in pub_j and pub_j["crossref"] == venue_i["key"]:
                     punti = punti + pub_j["score"]
             print("\n\t" + Back.MAGENTA + Fore.BLACK + "\tResult #" + str(i + 1) + "\t", end="\t")
-            if self.user_score:
+            if user_score:
                 print(Style.BRIGHT + Fore.LIGHTMAGENTA_EX + "Total Score: " + Style.BRIGHT + Fore.BLACK + str(
                     round(punti, 3)))
-            self.print_element(venue_i, -1)
+            print_element(venue_i, -1, user_score)
+            printed_counter = 1
             for j in range(len(threshold_results)):
                 pub_j = threshold_results[j]["p"]
                 if pub_j != None and "crossref" in pub_j and pub_j["crossref"] == venue_i["key"]:
                     punti = punti + pub_j["score"]
-                    self.print_element(pub_i, j)
+                    print_element(pub_j, printed_counter, user_score)
+                    printed_counter = printed_counter + 1
             threshold_results[:] = [d for d in threshold_results if d.get('v') != venue_i]
         elif pub_i != None:
             print("\n\t" + Back.MAGENTA + Fore.BLACK + "\tResult #" + str(i + 1) + "\t", end="\t")
-            if self.user_score:
+            if user_score:
                 print(Style.BRIGHT + Fore.LIGHTMAGENTA_EX + "Total Score: " + Style.BRIGHT + Fore.BLACK + str(
                     round(pub_i["score"], 3)))
-            self.print_element(pub_i, -1)
+            print_element(pub_i, -1, user_score)
             threshold_results[:] = [d for d in threshold_results if d.get('p') != pub_i]
 
         i = i + 1
 
 
-def print_results(self, results_set):
+def print_results(results_set, user_output_results, user_score):
     if len(results_set) > 0:
         results_shown = 0
         print()
         for result in results_set:
-            if results_shown < self.user_output_results:
+            if results_shown < user_output_results:
                 print("\n\t" + Back.MAGENTA + Style.BRIGHT + Fore.BLACK + "\tResult #" + str(results_shown + 1) + "\t",
                       end="\t")
-                if self.user_score:
+                if user_score:
                     print(Style.BRIGHT + Fore.LIGHTMAGENTA_EX + "Score:\t" + Style.BRIGHT + Fore.BLACK + str(
                         round(result['score'], 3)), end="")
                 print()
-                self.print_element(result, -1)
+                print_element(result, -1, user_score)
                 results_shown += 1
     else:
         print(Style.BRIGHT + Fore.MAGENTA + "\tNo result found")
 
 
-def print_element(self, element, j):
-    if not self.user_score:
+def print_element(element, j, user_score):
+    if not user_score:
         print()
     if element["pubtype"] in Parsing.publications:
         if j >= 0:
-            print(Style.BRIGHT + Fore.MAGENTA + "\n\t\tPUBLICATION #" + str(j + 1))
+            print(Style.BRIGHT + Fore.MAGENTA + "\n\t\tPUBLICATION #" + str(j))
         else:
             print(Style.BRIGHT + Fore.MAGENTA + "\n\t\tPUBLICATION")
     elif element["pubtype"] in Parsing.venues or element["pubtype"] == "journal":
